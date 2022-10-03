ea97eb3fc2ada78921cce82fa04318ab34d528be
th3-rocha <murilorocha537@gmail.com>
2022-10-03 14:55:34 -0300

    c

--- countrys/src/App.js
+++ countrys/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> loucura teste.
+          Edit 2 <code>src/App.js</code> loucura teste 2.
         </p>
         <a
           className="App-link"
