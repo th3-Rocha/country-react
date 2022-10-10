a787f842c6497cd79e00dbe0d1aca295fae1f8a4
th3-rocha <murilorocha537@gmail.com>
2022-10-10 16:00:39 -0300

    c

--- src/Home.js
+++ src/Home.js
@@ -1,97 +1,100 @@
 
 import './Home.css';
 
-import { HiSearch } from "react-icons/hi";
-
 import { Component } from 'react';
 
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: "https://restcountries.com/v3.1/name/peru"
-})
+import React,{useEffect} from 'react';
 
-class Home extends Component{
+import { HiSearch } from "react-icons/hi";
 
 
-  state = {
-    name:[]
+    
+  
+
+  class Home extends Component{
+
+    Apiget(){
+      CoutryName = "peru";
+      let CountryData;
+      axios.get("https://restcountries.com/v3.1/name/" + CoutryName)
+      .then((response) =>{
+        CountryData = response.data[0];
+        console.log(CountryData);
+      });
+    }
+    
+    render(){
+      return (
+        Apiget();
+        <div className="containerThree">
+          <div className="header">
+            <h2>Where in the World?</h2>
+            <h3>Dark Mode</h3>
+          </div>
+          <div className="menu"> 
+            <div className="search-container">
+              <HiSearch className='search-icon'/>
+              <input className="search" type="text" name="SearchInput"  placeholder="Search for a country..." />
+            </div>
+            <select className="regions">
+              <option value="" selected >Filter by Region</option>
+              <option value="africa">Africa</option>
+              <option value="america">America</option>
+              <option value="asia">Asia</option>
+              <option value="europe">Europe</option>
+              <option value="oceania">Oceania</option>
+            </select>
+          </div>
+          <div className="gridPlace">
+            <div className='flagContainer'>
+                <img className='CountryImg' >
+                
+                </img>
+                <h3 className='CountryName'>
+  
+                </h3>
+                <div className='CountryInfo'>
+                  <h4>Population:</h4>
+                  
+  
+                  <h4>Region:</h4>
+                  <h4>Capital:</h4>
+                
+                </div>
+                
+            </div>
+            <div className='flagContainer'>
+    
+                
+            </div>
+            <div className='flagContainer'>
+    
+                
+            </div>
+            <div className='flagContainer'>
+    
+                
+            </div>
+            <div className='flagContainer'>
+    
+                
+            </div>
+            <div className='flagContainer'>
+    
+                
+            </div>
+            
+          </div>
+        </div>
+        
+      );
+    }
+    
+
+
   }
 
-
-  constructor(){
-    super();
-    api.get('/').then(res => {
-      console.log(res.data)
-      this.setState({name:res.data})
-    })
-  }
-
-
-  render(){
-    return (
-      <div className="containerThree">
-        <div className="header">
-          <h2>Where in the World?</h2>
-          <h3>Dark Mode</h3>
-        </div>
-        <div className="menu"> 
-          <div className="search-container">
-            <HiSearch className='search-icon'/>
-            <input className="search" type="text" name="SearchInput"  placeholder="Search for a country..." />
-          </div>
-          <select className="regions">
-            <option value="" selected >Filter by Region</option>
-            <option value="africa">Africa</option>
-            <option value="america">America</option>
-            <option value="asia">Asia</option>
-            <option value="europe">Europe</option>
-            <option value="oceania">Oceania</option>
-          </select>
-        </div>
-        <div className="gridPlace">
-          <div className='flagContainer'>
-              <img className='CountryImg' >
-              
-              </img>
-              <h3 className='CountryName'>
-
-              </h3>
-              <div className='CountryInfo'>
-                <h4>Population:</h4>
-                <h4>Region:</h4>
-                <h4>Capital:</h4>
-              </div>
-              
-          </div>
-          <div className='flagContainer'>
-  
-              
-          </div>
-          <div className='flagContainer'>
-  
-              
-          </div>
-          <div className='flagContainer'>
-  
-              
-          </div>
-          <div className='flagContainer'>
-  
-              
-          </div>
-          <div className='flagContainer'>
-  
-              
-          </div>
-          
-        </div>
-      </div>
-      
-    );
-
-
-  }
-}
 
 export default Home;
